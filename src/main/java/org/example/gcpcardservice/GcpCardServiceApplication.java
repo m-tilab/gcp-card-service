80b21de128cfc1a978807b0e77cde68c085197b0
mehdi.tilab <mehdi.tilab@virtudev.net>
2024-05-12 16:22:37 +0200

    Initial commit

---
+++ src/main/java/org/example/gcpcardservice/GcpCardServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.gcpcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpCardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcpCardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gcpcardservice/GcpCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gcpcardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpCardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
